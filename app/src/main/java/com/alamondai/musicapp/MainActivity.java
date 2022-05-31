b25c3d73a25bf806da17fcbd65f8a9364b55c30e
nattynengeda@gmail.com <nattynengeda@gmail.com>
2022-05-31 13:04:42 -0400

    Third Commit.... The Player Works Completly

--- app/src/main/java/com/alamondai/musicapp/MainActivity.java
+++ app/src/main/java/com/alamondai/musicapp/MainActivity.java
@@ -2,13 +2,155 @@ package com.alamondai.musicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] items;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listview1);
+
+        runTimePermission();
+
     }
+    public void runTimePermission() {
+        Dexter.withContext(getApplicationContext())
+                .withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+
+                        //Method To Display songs
+                         displaySong();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+
+                }).check();
+    }
+
+    public ArrayList<File> findSong(File file) {
+
+        //ArrayList to store all songs
+        ArrayList<File> arrayList = new ArrayList<>();
+        File[] files = file.listFiles();
+
+        for (File singleFile : files) {
+
+            //Adding the directory to arrayList if it is not hidden
+            if (singleFile.isDirectory() && !singleFile.isHidden()) {
+
+                arrayList.addAll(findSong(singleFile));
+
+            } else {
+                //Adding the single music file to ArrayList
+                if (singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav")) {
+                    arrayList.add(singleFile);
+                }
+            }
+        }
+
+        return arrayList;
+    }
+    public void displaySong() {
+
+        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
+        items = new String[mySongs.size()];
+
+        //Adding all the music file without extensions to ArrayList
+        for (int i = 0; i < mySongs.size(); i++) {
+            items[i] = mySongs.get(i).getName().toString().replace(".mp3", "").replace(".wav", "");
+        }
+
+        //Calling the adapter and setting it to ListView
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
+
+        //Implementing onClickListener for ListView
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String songName = (String) listView.getItemAtPosition(i);
+
+                //Calling the next intent and sending the Required Details to play the songs
+                Intent intent = new Intent(getApplicationContext(), PlayerActivity.class);
+                intent.putExtra("songs", mySongs);
+                intent.putExtra("songname", songName);
+                intent.putExtra("pos", i);
+                startActivity(intent);
+            }
+        });
+    }
+
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            //Returning the count of total songs in an ArrayList
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            //Inflating all the single music files in a Layout File
+            View view = getLayoutInflater().inflate(R.layout.song_name, null);
+            TextView txtSong = view.findViewById(R.id.SongName);
+            txtSong.setSelected(true);
+            txtSong.setText(items[position]);
+            return view;
+        }
+    }
+
+
+
+
+
+
 }
